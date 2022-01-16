dbb66b6a25ff5fbec2ba31ba63fd89fca9cd4be5
MfundoZA <jordantenza@gmail.com>
2022-01-16 13:16:41 +0200

    Setup connection between Activity and NoteViewModel

--- app/src/main/java/com/mfundoza/mynotes/MainActivity.java
+++ app/src/main/java/com/mfundoza/mynotes/MainActivity.java
@@ -1,14 +1,33 @@
 package com.mfundoza.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.mfundoza.mynotes.models.Note;
+import com.mfundoza.mynotes.viewmodels.NoteViewModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // Update our RecyclerView
+                Snackbar.make(findViewById(R.id.main_constraint_layout), "OnChange", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
