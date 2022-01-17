df8b7bbbe51c9a7ed728758ae6ef4ef6a64cd7bc
MfundoZA <jordantenza@gmail.com>
2022-01-17 13:29:12 +0200

    Finished RecyclerView setup

--- app/src/main/java/com/mfundoza/mynotes/MainActivity.java
+++ app/src/main/java/com/mfundoza/mynotes/MainActivity.java
@@ -3,10 +3,13 @@ package com.mfundoza.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.mfundoza.mynotes.adapters.NoteAdapter;
 import com.mfundoza.mynotes.models.Note;
 import com.mfundoza.mynotes.viewmodels.NoteViewModel;
 
@@ -21,12 +24,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.rcyNotes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 // Update our RecyclerView
-                Snackbar.make(findViewById(R.id.main_constraint_layout), "OnChange", Snackbar.LENGTH_LONG).show();
+                adapter.setNotes(notes);
+
+                //Snackbar.make(findViewById(R.id.main_constraint_layout), "OnChange", Snackbar.LENGTH_LONG).show();
             }
         });
     }
