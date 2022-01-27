24611a7d2bf6ed3e466ddeeb3f4fa8fc37d580ca
Mfundo <jordantenza@gmai.com>
2022-01-27 16:50:10 +0200

    Set up view binding in AddNoteActivity

--- app/src/main/java/com/mfundoza/mynotes/AddNoteActivity.java
+++ app/src/main/java/com/mfundoza/mynotes/AddNoteActivity.java
@@ -3,12 +3,15 @@ package com.mfundoza.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddNoteActivity extends AppCompatActivity {
+    private AddNoteBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_note);
+        binding = AddNoteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
