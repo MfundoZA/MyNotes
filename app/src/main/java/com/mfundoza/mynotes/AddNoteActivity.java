c57fa0fd2e30b018490bd77bc3150aaad35809d9
Mfundo <jordantenza@gmai.com>
2022-02-07 12:35:18 +0200

    Created method to validate form input in AddNoteActivity

--- app/src/main/java/com/mfundoza/mynotes/AddNoteActivity.java
+++ app/src/main/java/com/mfundoza/mynotes/AddNoteActivity.java
@@ -5,13 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.mfundoza.mynotes.databinding.ActivityAddNoteBinding;
+
 public class AddNoteActivity extends AppCompatActivity {
-    private AddNoteBinding binding;
+    private ActivityAddNoteBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = AddNoteBinding.inflate(getLayoutInflater());
+        binding = ActivityAddNoteBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.nmbPriority.setMinValue(1);
+        binding.nmbPriority.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+
+        binding.btnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+    }
+
+    private void saveNote() {
+        String title = binding.edtTitle.getText().toString();
+        String description = binding.edtDescription.getText().toString();
+        int priority = binding.nmbPriority.getValue();
+
+        if (title.trim().isEmpty() || description.trim().isEmpty()) {
+            Snackbar.make(binding.getRoot(), "Please insert a title and description", Snackbar.LENGTH_SHORT);
+
+            return;
+        }
+
+
     }
 }
