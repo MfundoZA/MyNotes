b9b1ba7622e0ec7028583fb021f17919ab8b4c16
Mfundo <jordantenza@gmai.com>
2022-02-28 10:19:50 +0200

    Repository for adding notes

--- app/src/main/java/com/mfundoza/mynotes/repositories/AddNoteRepository.java
+++ app/src/main/java/com/mfundoza/mynotes/repositories/AddNoteRepository.java
@@ -1,4 +1,35 @@
 package com.mfundoza.mynotes.repositories;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import com.mfundoza.mynotes.daos.NoteDao;
+import com.mfundoza.mynotes.models.Note;
+import com.mfundoza.mynotes.roomdatabase.NoteDatabase;
+
 public class AddNoteRepository {
+    private NoteDao noteDao;
+
+    public AddNoteRepository(Application application) {
+        NoteDatabase database = NoteDatabase.getInstance(application);
+        noteDao = database.noteDao();
+    }
+
+    public void insert(Note note) {
+        new AddNoteRepository.InsertNoteAsyncTask(noteDao).execute(note);
+    }
+
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        private  InsertNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert((notes[0]));
+            return null;
+        }
+    }
 }
--- app/src/main/java/com/mfundoza/mynotes/viewmodels/AddNoteViewModel.java
+++ app/src/main/java/com/mfundoza/mynotes/viewmodels/AddNoteViewModel.java
@@ -1,4 +1,26 @@
 package com.mfundoza.mynotes.viewmodels;
 
-public class AddNoteViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.mfundoza.mynotes.models.Note;
+import com.mfundoza.mynotes.repositories.AddNoteRepository;
+import com.mfundoza.mynotes.repositories.MainRepository;
+
+public class AddNoteViewModel extends AndroidViewModel {
+
+    private AddNoteRepository repository;
+
+    public AddNoteViewModel(@NonNull Application application) {
+        super(application);
+
+        repository = new AddNoteRepository(application);
+
+    }
+
+    public void insert(Note note) {
+        repository.insert(note);
+    }
 }
