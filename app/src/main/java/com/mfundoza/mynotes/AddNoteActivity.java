1acf85030fe460ee9a671608836fb7b6f38324b7
Mfundo <jordantenza@gmai.com>
2022-02-28 10:18:40 +0200

    Added FloatingActionButton

--- app/src/main/java/com/mfundoza/mynotes/AddNoteActivity.java
+++ app/src/main/java/com/mfundoza/mynotes/AddNoteActivity.java
@@ -1,15 +1,20 @@
 package com.mfundoza.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.mfundoza.mynotes.databinding.ActivityAddNoteBinding;
+import com.mfundoza.mynotes.models.Note;
+import com.mfundoza.mynotes.viewmodels.AddNoteViewModel;
 
 public class AddNoteActivity extends AppCompatActivity {
+
     private ActivityAddNoteBinding binding;
+    private AddNoteViewModel addNoteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class AddNoteActivity extends AppCompatActivity {
                 saveNote();
             }
         });
+
+        addNoteViewModel = new ViewModelProvider(this).get(AddNoteViewModel.class);
     }
 
     private void saveNote() {
@@ -37,11 +44,13 @@ public class AddNoteActivity extends AppCompatActivity {
         int priority = binding.nmbPriority.getValue();
 
         if (title.trim().isEmpty() || description.trim().isEmpty()) {
-            Snackbar.make(binding.getRoot(), "Please insert a title and description", Snackbar.LENGTH_SHORT);
+            Snackbar.make(binding.getRoot(), "Please insert a title and description", Snackbar.LENGTH_SHORT).show();
 
             return;
         }
 
-
+        Note newNote = new Note(title, description, priority);
+        addNoteViewModel.insert(newNote);
+        finish();
     }
 }
